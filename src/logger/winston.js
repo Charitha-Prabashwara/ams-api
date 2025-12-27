c17462fdaab2403f4721345668d5dc30b1b090e1
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2025-12-27 14:14:30 +0530

    create request logger

---
+++ src/logger/winston.js
@@ -0,0 +1,26 @@
+const winston = require('winston');
+const DailyRotateFile = require('winston-daily-rotate-file');
+
+const logFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.json()
+);
+
+const transport = new DailyRotateFile({
+  filename: 'logs/request/api-requests-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '30d'
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: logFormat,
+  transports: [
+    transport,
+    new winston.transports.Console() // remove in production if needed
+  ]
+});
+
+module.exports = logger;
