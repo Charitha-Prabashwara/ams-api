32095641053b47c6bf7b562ca5077a833f847e03
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2026-02-05 12:54:27 +0530

    router issue fixed

--- src/routes/subjectRegistration.routes/subjectRegistration.routes.js
+++ src/routes/subjectRegistration.routes/subjectRegistration.routes.js
@@ -6,11 +6,12 @@ const {CreateSubjectRegistrationDTO, GetSubjectRegistrationByIdDTO, UpdateSubjec
 const {createSubjectRegistration} = require('../../controllers/subjectRegistration.controllers/createSubjectRegistration.controller')
 const {getSubjectRegistrationById, getFindSubjectRegistrations} = require('../../controllers/subjectRegistration.controllers/get.subjectRegistration.controller')
 const {updateSubjectRegistrationById} = require('../../controllers/subjectRegistration.controllers/update.subjectRegistration.controller')
+const {deleteSubjectRegistrationById} = require('../../controllers/subjectRegistration.controllers/delete.subjectRegistration.controller')
 const subjectRegistrationRouter = express.Router()
 
 subjectRegistrationRouter.post('/', withDTO(CreateSubjectRegistrationDTO, createSubjectRegistration))
 subjectRegistrationRouter.post('/id/', withDTO(GetSubjectRegistrationByIdDTO, getSubjectRegistrationById))
 subjectRegistrationRouter.put('/id/', withDTO(UpdateSubjectRegistrationByIdDTO, updateSubjectRegistrationById))
 subjectRegistrationRouter.post('/find/', withDTO(GetFindSubjectRegistrationByIdDTO, getFindSubjectRegistrations))
-subjectRegistrationRouter.delete('/id/', withDTO(DeleteSubjectRegistrationByIdDTO, getFindSubjectRegistrations))
+subjectRegistrationRouter.delete('/id/', withDTO(DeleteSubjectRegistrationByIdDTO, deleteSubjectRegistrationById))
 module.exports = subjectRegistrationRouter
